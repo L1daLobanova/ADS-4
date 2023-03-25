1720cb3f9bb84875619f6d39ed5f7403d0205309
L1daLobanova <124993574+L1daLobanova@users.noreply.github.com>
2023-03-25 17:03:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,51 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int k = 0;
+  for (int i = 0; i < len - 1; i++)
+    for (int j = 0; j < len; j++)
+      if ((arr[i] + arr[j]) == value)
+        k += 1;
+  return k;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int k = 0;
+  for (int i = 0; i < len - 1; i++){
+    if (arr[i] > value)
+      continue;
+    for (int j = len; j > i; j--){
+      if (arr[j] > value)
+        continue;
+      if ((arr[i] + arr[j]) == value)
+        k += 1;
+    }
+  }
+  return k;      
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int k = 0;
+  for (int i = 0; i < len - 1; i++){
+    int start = i, end = len, nado = value - arr[i];
+    while ((end - start)>1){
+      int mid = (start+end)/2;
+      if (arr[mid] == nado){
+        k++;
+        int top = mid + 1;
+        while (arr[top] == nado && top < end){
+          k += 1;
+          top++;
+        }
+        top = mid - 1;
+        while (arr[top] == nado && top > start){
+          k += 1;
+          top--;
+        }
+        break;
+      }
+      if (arr[mid] > nado)
+        end = mid;
+      if (arr[mid] > nado)
+        start = mid;
+    }
+  }
+  return k;
 }
